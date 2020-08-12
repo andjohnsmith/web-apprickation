4f59ec010082f6125c713228c088814db21016bb
Andrew Smith <smithandrewj@outlook.com>
2020-08-11 18:13:55 -0700

    added grid and search

--- src/App.js
+++ src/App.js
@@ -1,8 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Header from './components/ui/Header';
+import Search from './components/ui/Search';
+import CharacterGrid from './components/characters/CharacterGrid';
 import './App.css';
 
 const App = () => {
-  return <div className="container">Hello</div>;
+  const [items, setItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [query, setQuery] = useState('');
+
+  useEffect(() => {
+    const fetchItems = async () => {
+      const result = await axios(
+        `https://rickandmortyapi.com/api/character/?name=${query}`,
+      );
+
+      setItems(result.data.results);
+      setIsLoading(false);
+    };
+
+    fetchItems();
+  }, [query]);
+
+  return (
+    <div className="container">
+      <Header />
+      <Search getQuery={(q) => setQuery(q)} />
+      <CharacterGrid isLoading={isLoading} items={items} />
+    </div>
+  );
 };
 
 export default App;
